e342803eef75d8e32c9076e7dbccd07637c3a391
AdamWallgren28 <mattef91@hotmail.com>
2024-02-27 19:07:04 +0100

    sets html semantics & removes old JS

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-let burgerMenu = document.getElementById('burgerMenu');
-let nav = document.querySelector('.navBox');
-
-burgerMenu.addEventListener('click', function() {
-    
-    if (nav.style.display === 'none' || nav.style.display === '') {
-        nav.style.display = 'block';
-    } else {
-        nav.style.display = 'none';
-    }
-});
-
+//mailKnapp
 function openMail() {
     var emailAddress = "mattef91@hotmail.com"; 
     window.location.href = "mailto:" + emailAddress;
